ef23a0ab77e0a95ea3e450d56615896d7ceee052
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-01-16 19:26:15 +0900

    correct errors

--- paint/src/paint.java
+++ paint/src/paint.java
@@ -1,14 +1,10 @@
-/**
- * Created by Ryoto on 2015/01/16.
- */
-
 /*import java.awt.*;
 import java.awt.event.*;
 */
 import javax.swing.*;
 
 public class paint extends JFrame {
-    public static void main(String args) {
+    public static void main(String[] args) {
         new paint();
     }
 
