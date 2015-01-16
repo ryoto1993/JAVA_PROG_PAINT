004441dfe579bd2cd90e3694827c72e20821fb97
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-01-16 19:21:48 +0900

    import swing

---
+++ paint/src/paint.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Ryoto on 2015/01/16.
+ */
+
+/*import java.awt.*;
+import java.awt.event.*;
+*/
+import javax.swing.*;
+
+public class paint extends JFrame {
+    public static void main(String args) {
+        new paint();
+    }
+
+    public paint() {
+        this.setSize(300, 230);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setTitle("Simple Paint");
+        this.setVisible(true);
+    }
+}
