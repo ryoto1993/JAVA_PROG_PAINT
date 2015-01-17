c523f166291c55ba72add6fb4b2ca335d2c13d70
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-01-18 03:03:01 +0900

    look and feel

--- paint/src/paint.java
+++ paint/src/paint.java
@@ -9,6 +9,18 @@ public class paint extends JFrame {
     }
 
     public paint() {
+        // set look and feel
+        try {
+            UIManager
+                    .setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException ignored) {
+        } catch (InstantiationException e) {
+        } catch (IllegalAccessException e) {
+        } catch (UnsupportedLookAndFeelException e) {
+        }
+        SwingUtilities.updateComponentTreeUI(this);
+
+        // set frame
         this.setSize(300, 230);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setTitle("Simple Paint");
